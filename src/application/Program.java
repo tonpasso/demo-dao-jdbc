d8a4313b7aba6a77ca18ac75d200269f3f4ac50a
Wellington Passo <wellpasso@gmail.com>
2020-05-19 15:08:07 -0300

    Reusando Instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,8 +14,8 @@ public class Program {
 				
 		SellerDao sellerDao = DaoFactory.createSellerDao();		
 		
+		System.out.println("=== Teste 1: seller findById ===");
 		Seller seller = sellerDao.findById(4);
-		
 		System.out.println(seller);
 
 	}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -53,18 +53,22 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if(rs.next()) {
-				Department dep = new Department();				
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
 				
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("DepartmentId"));
-				obj.setName(rs.getString("Nome"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+				Department dep = instantiateDepartment(rs);				
+				Seller obj = instantiateSeller(rs, dep);
 				return obj;
+				
+//				Department dep = new Department();				
+//				dep.setId(rs.getInt("DepartmentId"));
+//				dep.setName(rs.getString("DepName"));
+//				
+//				Seller obj = new Seller();
+//				obj.setId(rs.getInt("DepartmentId"));
+//				obj.setName(rs.getString("Nome"));
+//				obj.setEmail(rs.getString("Email"));
+//				obj.setBaseSalary(rs.getDouble("BaseSalary"));
+//				obj.setBirthDate(rs.getDate("BirthDate"));
+//				obj.setDepartment(dep);				
 			}
 			return null;
 		}
@@ -77,6 +81,25 @@ public class SellerDaoJDBC implements SellerDao {
 			DB.closeResultSet(rs);
 		}
 		
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("DepartmentId"));
+		obj.setName(rs.getString("Nome"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
+		
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();				
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
